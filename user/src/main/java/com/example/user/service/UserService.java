2b8dfd71ac1f29d282a8bbf6a94f387af1d309d6
王强 <wangq@bfwlkj.com>
2023-03-06 14:59:26 +0800

    feat(test): test

---
+++ user/src/main/java/com/example/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String getUser() {
+        return "lxddg";
+    }
+}
